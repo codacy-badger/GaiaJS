992ecb05033059db8f7d6d40a43733bf5d0e5701
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2018-08-22 13:24:49 +0200

    renamed icon.css, timestamp.css; added inlineText to timestamp

--- src/lib/components/Timestamp.ts
+++ src/lib/components/Timestamp.ts
@@ -1,15 +1,16 @@
-import "./assets/text_time.css";
+import {InlineText} from "./InlineText";
+
+import "./assets/timestamp.css";
 
 export class Timestamp {
 
     public static render() {
         const date = new Date();
         const f = (e: any) => e.toString().padStart(2, "0");
-
-        const textTime = document.createElement("span");
-        textTime.classList.add("text-time");
-        textTime.appendChild(document.createTextNode(f(date.getHours()) + ":" + f(date.getMinutes())));
-
-        return textTime;
+        const text = f(date.getHours()) + ":" + f(date.getMinutes());
+        const timestamp = document.createElement("span");
+        timestamp.classList.add("timestamp");
+        timestamp.appendChild(new InlineText(text).render());
+        return timestamp;
     }
 }
