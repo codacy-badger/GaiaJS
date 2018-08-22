571055a76f93cedfb51d97a5177f849cb52d985d
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2018-08-22 11:41:42 +0200

    removed test ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // import `.scss` files
 // import Gaia class
-// import { Gaia as defaultExport } from "./lib/gaia";
 import { GaiaChannel as defaultExport} from "./lib/GaiaChannel";
 import "./scss/styles.scss";
 // export default Gaia class
