eb34b92e98323f899119410823275370ab120be7
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2018-08-22 11:50:26 +0200

    added support for branches other than master

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+    branch_name="$TRAVIS_PULL_REQUEST_BRANCH"
+else
+    branch_name="$TRAVIS_BRANCH"
+fi
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 commit_build_files() {
-  git checkout master
+  git checkout ${branch_name}
   git add -f dist
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
@@ -14,7 +20,7 @@ commit_build_files() {
 upload_files() {
   git remote rm origin
   git remote add origin https://MiniXC:${GITHUB_TOKEN}@github.com/MiniXC/GaiaJS.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+  git push --quiet --set-upstream origin ${branch_name}
 }
 
 setup_git
